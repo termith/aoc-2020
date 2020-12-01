bab36f1022e4677606cba96b75e3829b84a0adb9
termith <dmitdemidov@gmail.com>
2020-12-01 16:43:49 +0300

    day1

---
+++ README.md
@@ -0,0 +1,4 @@
+# Advent of Code 2020 
+[Link](https://adventofcode.com/)
+
+Just for fun and practice
---
+++ day1/solution.py
@@ -0,0 +1,30 @@
+"""
+    P1. Find two numbers in array which sum is given number (2020)
+    P2. Do the same for three numbers
+"""
+with open('input') as f:
+    numbers = sorted(list(map(int, f.readlines())))
+
+
+def find_sum(array, target=2020):
+    i = 0
+    j = len(array) - 1
+
+    while i < j:
+        s = array[i] + array[j]
+        if s > target:
+            j -= 1
+        elif s < target:
+            i += 1
+            j = len(array) - 1
+        else:
+            return array[i], array[j]
+    return None
+
+
+for idx in range(len(numbers)):
+    result = find_sum(numbers[idx + 1:], 2020 - numbers[idx])
+    if result is not None:
+        print(numbers[idx], *result)
+        print(result[0] * result[1] * numbers[idx])
+        break
